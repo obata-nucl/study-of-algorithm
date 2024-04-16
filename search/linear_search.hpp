919da71dd4953b4de13e7f96d029e883cc37cba2
obata-nucl <obata0904@outlook.jp>
2024-04-16 17:25:25 +0900

    linear search

--- search/linear_search.hpp
+++ search/linear_search.hpp
@@ -1,6 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+
 double linear_search(vector<double> sample,double target){
     for(int i=0;i<sample.size();i++){
         if(sample[i] == target){
